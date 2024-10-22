3e3548078734f77a523ffe91fe2e43ad21d370f5
João Pedro Cordeiro <jpc5626@gmail.com>
2024-10-21 22:57:28 -0300

    comment db seed file 2

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -107,14 +107,14 @@
 //   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
 //   // });
 //   try {
-//     await client.sql`BEGIN`;
-//     await seedUsers();
-//     await seedCustomers();
-//     await seedInvoices();
-//     await seedRevenue();
-//     await client.sql`COMMIT`;
+// // await client.sql`BEGIN`;
+// // await seedUsers();
+// // await seedCustomers();
+// // await seedInvoices();
+// // await seedRevenue();
+// // await client.sql`COMMIT`;
 
-//     return Response.json({ message: 'Database seeded successfully' });
+// return Response.json({ message: 'Database seeded successfully' });
 //   } catch (error) {
 //     await client.sql`ROLLBACK`;
 //     return Response.json({ error }, { status: 500 });
